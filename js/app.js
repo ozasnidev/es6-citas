7c26b4013ac7c3a6699014e02853900350c88db1
Ozasnidev <ozasnidev@gmail.com>
2021-01-11 19:40:50 +0100

    03 - Data validation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const nuevaCitaForm = ("#nuevaCitaForm");
+const nuevaCitaForm = document.querySelector("#nuevaCitaForm");
 
 const pacienteInput = document.querySelector("#pacienteInput");
 const direccionInput = document.querySelector("#direccionInput");
@@ -11,6 +11,42 @@ const desktopContainer = document.querySelector("#desktopContainer");
 
 const citaObject = { paciente: '', direccion: '', telefono: '', dia: '', hora: '', sintomas: '' }
 
+class CitaList {
+    constructor(){
+        this.citasCollection = [];
+    }
+}
+
+class UI {
+    imprimirAlerta(mensaje) {
+        const mensajeContainer = document.createElement('article');
+        mensajeContainer.classList.add('message');
+        const mensajeHeader = document.createElement('div');
+        mensajeHeader.classList.add('message-header');
+        const mensajeBody = document.createElement('div');
+        mensajeBody.classList.add('message-body');
+        const mensajeHeaderParrafo = document.createElement('p');
+        const mensajeBodyParrafo = document.createElement('p');
+
+        mensajeContainer.classList.add('is-danger');
+        mensajeHeaderParrafo.textContent = "Error";
+        mensajeBodyParrafo.textContent = mensaje;
+        mensajeHeader.append(mensajeHeaderParrafo);
+        mensajeBody.append(mensajeBodyParrafo);
+        
+        mensajeContainer.appendChild(mensajeHeader);
+        mensajeContainer.appendChild(mensajeBody);
+
+        document.querySelector('#desktopContainer').appendChild(mensajeContainer);
+        setTimeout(()=>{
+            mensajeContainer.remove();
+        },5000);
+    }
+}
+
+const userInterface = new UI();
+const citasRepository = new CitaList();
+
 eventListeners();
 
 function eventListeners(){
@@ -20,9 +56,27 @@ function eventListeners(){
     diaRegistroInput.addEventListener('change', setDatosCita);
     horaRegistroInput.addEventListener('change', setDatosCita);
     sintomasInput.addEventListener('change', setDatosCita);
+
+    nuevaCitaForm.addEventListener('submit', validarCita);
 }
 
 function setDatosCita(event){
     citaObject[event.target.name] = event.target.value;
     console.log(citaObject);
+}
+
+function validarCita(event){
+    event.preventDefault();
+
+    const { paciente, direccion, telefono, dia, hora, sintomas } = citaObject;
+
+    if( paciente === '' || 
+        direccion === '' || 
+        telefono === '' || 
+        dia === '' || 
+        hora === '' ||
+        sintomas === ''){
+            userInterface.imprimirAlerta('Falta informacion');
+            return;           
+        } 
 }
